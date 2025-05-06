d1235dcb02764a4870a88b60d30d732f70fe5ad1
krunchiekrunch <75719839+RadioactivePotato@users.noreply.github.com>
2025-05-06 11:58:43 +0100

    license update for userscripts

--- groundspeed_only-userscript.js
+++ groundspeed_only-userscript.js
@@ -7,7 +7,7 @@
 // @match        https://*.geo-fs.com/geofs.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
 // @grant        none
-// @license      GPL-3.0
+// @license      CC BY-NC-SA 4.0
 // ==/UserScript==
 
 // This userscript only display ground speed
